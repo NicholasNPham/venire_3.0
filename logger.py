6516ddeacc07d629dac6cb636340124e0fdfc374
NicholasNPham <npham@sao10.com>
2026-05-04 16:53:50 -0400

    added new func

--- logger.py
+++ logger.py
@@ -5,6 +5,15 @@ Purpose: Configures and returns the logger for Venire Automation
 
 # STANDARD LIBRARY IMPORTS
 import logging
+import os
 from datetime import date
 
+# CONSTANTS
+RESULTS_ROOT = 'results'
+LOG_FILE = os.path.join(RESULTS_ROOT, date.today().strftime('%Y-%m-%d')) + '.log'
+
+# FUNCTIONS
+
+def setup_logger() -> logging.Logger:
+    pass
 
