559180b91bcabecea7add8880566a5e45245e099
NicholasNPham <npham@sao10.com>
2026-05-07 10:45:03 -0400

    new function

--- config.py
+++ config.py
@@ -65,7 +65,53 @@ def load_config(json_file: str) -> AppConfig:
         config = load_config("config.json")
         config.browser.login.username_field  # "loginForm:username"
     """
-    with open(json_file, "r") as json_data:
-        data = json.load(json_data)
+    try:
+        with open(json_file, "r") as json_data:
+            data = json.load(json_data)
 
-    pass
+        browser_data = data["Browser"]
+
+        login_data = browser_data["Login"]
+        search_data = browser_data["Search"]
+        results_data = browser_data["Results"]
+        navigation_data = browser_data["Navigation"]
+
+        login_config = LoginConfig(
+            username_field=login_data["username_field"],
+            password_field=login_data["password_field"],
+            submit_login_button=login_data["submit_login_button"],
+        )
+
+        search_config = SearchConfig(
+            exclude_attorneys_checkbox=search_data["exclude_attorneys_checkbox"],
+            exclude_judges_checkbox=search_data["exclude_judges_checkbox"],
+            last_name_field=search_data["last_name_field"],
+            first_name_field=search_data["first_name_field"],
+            dob_field=search_data["dob_field"],
+            search_button=search_data["search_button"],
+            no_results_popup_message=search_data["no_results_popup_message"],
+        )
+
+        results_config = ResultsConfig(
+            view_selection_button=results_data["view_selection_button"],
+        )
+
+        navigation_config = NavigationConfig(
+            back_button_from_pdf_page=navigation_data["back_button_from_pdf_page"],
+            back_button_from_selection_page=navigation_data["back_button_from_selection_page"],
+            reset_button=navigation_data["reset_button"],
+        )
+
+        browser_config = BrowserConfig(
+            login=login_config,
+            search=search_config,
+            results=results_config,
+            navigation=navigation_config
+        )
+
+        app_config = AppConfig(browser=browser_config)
+        return app_config
+    except FileNotFoundError:
+        raise FileNotFoundError(f"config file not found: {json_file}")
+    except KeyError as e:
+        raise KeyError(f"Missing key in config.json: {e}")
